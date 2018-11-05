2a75044044c1642cbae506f133347f0ab702cff4
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-11-05 20:26:18 +0530

    Added script to run several games and note data

---
+++ testBench/runGames.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#./runGames.sh ./run.sh ./run.sh 2
+mkdir selfPlay
+source env/bin/activate
+COUNTER=0
+while [  $COUNTER -lt $3 ]; 
+    do
+        python server.py 10000 -n 5 -NC 2 -TL 150 -LOG selfPlay/gameRun.log > selfPlay/serverData_run$COUNTER.log &
+        sleep 0.5
+        python client.py 0.0.0.0 10000 $1 -mode GUI > selfPlay/player1_run$COUNTER.log &
+        sleep 0.5
+        python client.py 0.0.0.0 10000 $2 > selfPlay/player2_run$COUNTER.log
+        let COUNTER=COUNTER+1 
+    done
+deactivate
